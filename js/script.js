39d577ec67240ff50a857fdce56fc0c304171be3
Matteo Gioele Cocomazzi <TheZefyro@gmail.com>
2022-01-21 15:40:19 +0100

    finito

--- js/script.js
+++ js/script.js
@@ -9,25 +9,25 @@ let containerElement = document.getElementById('container');
 // Ciclo dei numeri da 1 - 100
 for (let i=1; i <=100; i++){
     
-    //Condizione multipli di 3 e 5
+    //Condizione numeri multipli di 3 e 5
     if (i % 3 == 0 && i % 5 == 0){
         console.log("FizzBuzz");
         containerElement.innerHTML = containerElement.innerHTML + "<div class='box4'>" + "FizzBuzz" + "</div>";
     } 
     
-    // Condizione multipli di 3
+    // Condizione numeri multipli di 3
     else if (i % 3 == 0){
         console.log("Fizz");
         containerElement.innerHTML = containerElement.innerHTML + "<div class='box2'>" + "Fizz" + "</div>";
     }
 
-    // Condizione multipli di 5 
+    // Condizione numeri multipli di 5 
     else if (i % 5 == 0){
         console.log("Buzz");
         containerElement.innerHTML = containerElement.innerHTML + "<div class='box3'>" + "Buzz" + "</div>";
     }
 
-    // Condizione per il resto dei numeri 
+    // Numeri restanti fuori dalle condizioni 
     else {
         console.log(i);
         containerElement.innerHTML = containerElement.innerHTML + "<div class='box1'>" + i + "</div>";
