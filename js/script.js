96c1910b3763c191bef9aefdc1c47c295be6f10e
Matteo Gioele Cocomazzi <TheZefyro@gmail.com>
2022-01-21 14:13:34 +0100

    push programma funzionante

--- js/script.js
+++ js/script.js
@@ -3,3 +3,17 @@
 // MILESTONE 2 Dato un container nel DOM, appendi un elemento html con il numero o la stringa corretta. 
 // MILESTONE 3 Applica uno stile differente a seconda del valore dell’indice per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5.
 
+let containerElement = document.getElementById('container');
+
+for (i=1; i <=100; i++){
+    
+    if (i % 3 == 0 && i % 5 ==0){
+        console.log("FizzBuzz");
+    } else if (i % 3 ==0){
+        console.log("Fizz");
+    } else if (i % 5 == 0){
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }     
+}
